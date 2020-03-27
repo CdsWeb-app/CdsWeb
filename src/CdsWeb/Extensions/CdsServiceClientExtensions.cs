bff9e6a60434d288b6078f70b8a1418fe29c5413
Colin Vermander <colin.vermander@adoxio.com>
2020-03-27 00:37:41 -0400

    Move logging to wrapper class

--- src/CdsWeb/Extensions/CdsServiceClientExtensions.cs
+++ src/CdsWeb/Extensions/CdsServiceClientExtensions.cs
@@ -13,10 +13,21 @@ namespace CdsWeb.Extensions
     /// </summary>
     public class CdsServiceClientWrapper
     {
+
         public readonly CdsServiceClient CdsServiceClient;
 
-        public CdsServiceClientWrapper(string connectionString)
+        public CdsServiceClientWrapper(string connectionString, ILogger<CdsServiceClientWrapper> logger, string traceLevel)
         {
+            TraceControlSettings.TraceLevel = 
+                (System.Diagnostics.SourceLevels)Enum.Parse(
+                    typeof(System.Diagnostics.SourceLevels), traceLevel);
+
+            TraceControlSettings.AddTraceListener(
+                new LoggerTraceListener(
+                    "Microsoft.PowerPlatform.Cds.Client", logger
+                    )
+                );
+
             CdsServiceClient = new CdsServiceClient(connectionString);
         }
     }
@@ -61,16 +72,11 @@ namespace CdsWeb.Extensions
             configureOptions(cdsServiceClientOptions);
 
             services.AddSingleton(sp =>
-                new LoggerTraceListener(
-                    "Microsoft.PowerPlatform.Cds.Client",
-                    sp.GetRequiredService<ILogger<LoggerTraceListener>>())
+                new CdsServiceClientWrapper(
+                    cdsServiceClientOptions.ConnectionString, 
+                    sp.GetRequiredService<ILogger<CdsServiceClientWrapper>>(), 
+                    cdsServiceClientOptions.TraceLevel)
                 );
-
-            TraceControlSettings.TraceLevel = (System.Diagnostics.SourceLevels)Enum.Parse(typeof(System.Diagnostics.SourceLevels), cdsServiceClientOptions.TraceLevel);
-            TraceControlSettings.AddTraceListener(services.BuildServiceProvider().GetRequiredService<LoggerTraceListener>());
-
-            services.AddSingleton(sp =>
-                new CdsServiceClientWrapper(cdsServiceClientOptions.ConnectionString));
 
             services.AddTransient<IOrganizationService, CdsServiceClient>(sp =>
                 sp.GetService<CdsServiceClientWrapper>().CdsServiceClient.Clone());
